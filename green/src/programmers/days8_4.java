8969710d0d5670c184d33cd96d9076e86097e9b8
allready-dawn <qwshpke@naver.com>
2023-06-07 19:34:51 +0900

    프로그래머스 8일차

--- green/src/programmers/days8_4.java
+++ green/src/programmers/days8_4.java
@@ -1,5 +1,5 @@
 package programmers;
-
+//정수가 담긴 리스트 num_list가 주어질 때, 모든 원소들의 곱이 모든 원소들의 합의 제곱보다 작으면 1을 크면 0을 return하도록 solution 함수를 완성해주세요.
 public class days8_4 {
 	public static int Solution(int[] num_list) {
         int answer = 0;
